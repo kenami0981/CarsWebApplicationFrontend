74a4e59b5f277afbcf9b03d7a341a67216e0a2a0
kenami0981 <amelia.janek.aj@gmail.com>
2025-11-26 13:07:28 +0100

    Style: NavBar + NotFound

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,18 +1,20 @@
 import { NavLink } from "react-router-dom";
+import "../Styles/NavBar.css";
+import autoIcon from "../Media/auto_icon_menu.png"
 
 export default function NavBar() {
     return (
-    <div>
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/cars">CARS</NavLink>
-                    <NavLink to="/add-car">ADD CAR</NavLink>
-                </li>
-                
-            </ul>
+        <nav className="navbar">
+            <div className="nav-container">
+                <img src={autoIcon} alt="car icon" className="nav-icon" />
+
+                <div className="nav-links">
+                    <NavLink to="/cars" className="nav-item">CARS</NavLink>
+                    <NavLink to="/add-car" className="nav-item">ADD CAR</NavLink>
+                </div>
+                <div className="nav-spacer"></div>
+            </div>
         </nav>
-    </div>
-        
+
     );
 }
--- src/Components/NotFound.tsx
+++ src/Components/NotFound.tsx
@@ -1,3 +1,25 @@
+import notFoundIcon from "../Media/not-found.png"
 export default function NotFound() {
-    return (<p>Ok</p>);
+    return (
+        <div style={{
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "20px",
+            textAlign: "center",
+            color: "#e0e0e0"
+        }}>
+            <h1 style={{ fontSize: "28px" }}>The page you're looking for doesn't exist.</h1>
+            <img 
+                src={notFoundIcon} 
+                alt="Not found icon"
+                style={{ width: "600px", opacity: 0.9 }}
+            />
+
+            
+        </div>
+    );
 }
